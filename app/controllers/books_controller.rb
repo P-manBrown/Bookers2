5ca6be00240144027f989954efab18421412e485
P-manBrown <tachikawa.yuya@outlook.jp>
2021-11-27 16:01:41 +0000

    [add] 4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,25 +9,40 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save(book_params)
+      redirect_to book_path(@book.id), success: 'You have created book successfully.'
+    else
+      @user = User.find(current_user.id)
+      @books = Book.page(params[:page])
+      render :index
+    end
   end
 
   def show
     @book_new = Book.new
     @book_find = Book.find(params[:id])
-    @user = User.find(current_user.id)
+    @user = @book_find.user
   end
-  
+
   def edit
     @book = Book.find(params[:id])
+    if @book.user.id == current_user.id
+      @book_id = Book.ids
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
-  
+
   def update
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id), success: 'You have update book successfully.'
+    else
+      @book_id = Book.ids
+      render :edit
+    end
   end
 
   def destroy
@@ -39,7 +54,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :body, :user_id)
+    params.require(:book).permit(:title, :body, :user_id )
   end
 
 end
